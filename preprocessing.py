e995dc5836c43b08f134e5603e60ac272232951a
amiruki <weeb.amiruddin@gmail.com>
2022-11-12 11:31:32 +0800

    cleanup'

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,3 @@
-import csv
-import os
 import psycopg2
 import json
 from tkinter import messagebox
@@ -57,30 +55,3 @@ class Preprocessing:
                 conn.close()
         print("success")
         return json_QEP, json_AQP
-
-    def tbl_to_csv(filename):
-        csv = open("".join([filename, ".csv"]), "w+")
-
-        tbl = open("".join([filename, ".tbl"]), "r")
-        lines = tbl.readlines()
-        for line in lines:
-            length = len(line)
-            line = line[:length - 2] + line[length-1:]
-            csv.write(line)
-        tbl.close()
-        csv.close()
-
-    # Main
-    filenames = [
-        "customer",
-        "lineitem",
-        "nation",
-        "orders",
-        "part",
-        "partsupp",
-        "region",
-        "supplier",
-    ]
-
-    # for filename in filenames:
-    #    tbl_to_csv(filename)
